649da68c18639dc215163f32a5369cf876d538a9
Zachary Langley <zaclangley04@gmail.com>
2020-08-22 16:14:37 -0600

    responsive images

--- src/components/RoomImage/index.js
+++ src/components/RoomImage/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import lightsOnImage from '../../assets/daytime_room.png';
-import lightsOffImage from '../../assets/nighttime_room.png';
+import lightsOnImageBig from '../../assets/daytime_room_768.png';
+import lightsOnImageSmall from '../../assets/daytime_room_200.png';
+import lightsOffImageBig from '../../assets/nighttime_room_768.png';
+import lightsOffImageSmall from '../../assets/nighttime_room_200.png';
 import './index.scss';
 
 const RoomImage = ({
     lightsOn
 }) => {
-    const image = (lightsOn) ? lightsOnImage : lightsOffImage
+    const lightImageSet = `${lightsOnImageSmall} 400w, ${lightsOnImageBig} 768w`;
+    const darkImageSet = `${lightsOffImageSmall} 400w, ${lightsOffImageBig} 768w`;
+    const image = (lightsOn) ? lightsOnImageSmall : lightsOffImageSmall
+    const imageSet = (lightsOn) ? lightImageSet : darkImageSet
 
-    return (<img src={image} alt={'room-art'}/>)
+    return (
+        <img src={image} srcset={imageSet} alt={'room-art'}/>
+    )
 }
 
 export default RoomImage;
