7e002fadb7266d5bdae899fd251dc7879d12a10a
Zachary Langley <zaclangley04@gmail.com>
2020-08-23 14:22:28 -0600

    replaced spotify embedded player with soundcloud

--- src/components/MusicPlayer/index.js
+++ src/components/MusicPlayer/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './index.scss';
 
+// <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/655383102&color=%237dbfc5&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/perfectmsc" title="Perfect" target="_blank" style="color: #cccccc; text-decoration: none;">Perfect</a> · <a href="https://soundcloud.com/perfectmsc/sets/lofi" title="Lofi?" target="_blank" style="color: #cccccc; text-decoration: none;">Lofi?</a></div>
+
 const MusicPlayer = () => {
     return (
-        <iframe title={"spotify_player_iframe"} src={"https://open.spotify.com/embed/playlist/0vvXsWCC9xrXsKd4FyS8kM"} width={"100%"} height={"100%"} frameBorder={"0"} allowtransparency={"true"} allow={"encrypted-media"}/>
+        <iframe width="100%" height="90" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/655383102&color=%237dbfc5&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"/>
     )
 };
 
